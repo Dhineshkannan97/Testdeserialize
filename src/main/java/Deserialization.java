742e1e1b1cd66374a2f5e26bb1eba3b87edae636
DhineshKannan97 <dhineshkannan.madhavan@gcitsolutions.com>
2022-11-04 18:58:02 +0530

    stage 2

--- src/main/java/Deserialization.java
+++ src/main/java/Deserialization.java
@@ -1,20 +1,11 @@
 import java.io.*;
 
-public class Deserialization implements Serializable {
-    private static final long serialVersionUID = -9223365651070458532L;
-    int age;
-    String name;
+public class Deserialization {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-
         String loc = "C:\\Users\\Dhinesh Kannan\\Downloads\\d.txt";
-        FileInputStream fileInputStream = new FileInputStream(loc); //new FileInputStream(loc)
-          BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
-          ObjectInputStream in = new ObjectInputStream(bufferedInputStream);
-//        byte[] bytes = in.readAllBytes();
-//        bytes.getClass();
-//        System.out.println(bytes.toString());
-       Deserialization der= (Deserialization) in.readObject();
-        System.out.println(der.age+der.name);
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream(loc));
+        TestSerial der = (TestSerial) in.readObject();
+        System.out.println(der.age +":"+ der.name+":" + der.id+":" + der.salary);
         in.close();
     }
 }
